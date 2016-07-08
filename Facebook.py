a80ee5ae6275c9946b57f1ba1f700ed1846a64cc
Gurupradeep <gurupradeept@gmail.com>
2016-07-08 22:40:32 +0530

    Facebook Login

---
+++ Facebook.py
@@ -0,0 +1,23 @@
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class Facebook_login(unittest.TestCase):
+
+	def setUp(self):
+		self.driver = webdriver.Firefox()
+
+	def test_whatsapp(self):
+		driver = self.driver
+		driver.get("https://www.facebook.com")
+		driver.maximize_window()
+		email = driver.find_element_by_xpath("//*[@id='email']")
+		email.send_keys('gurupradeept@gmail.com')
+		password = driver.find_element_by_xpath("//*[@id='pass']")
+		password.send_keys("9603982107")
+		Facebook_loginn_button = driver.find_element_by_xpath("//*[@id='u_0_m']")
+		login_button.click()
+	def tearDown(self):
+		self.driver.close()
+if __name__ == "__main__" :
+	unittest.main()
