4502c2f119938283b6314fb2242dc16a96226884
Gurupradeep <gurupradeept@gmail.com>
2016-12-11 11:59:32 +0530

    Update Facebook.py

--- Facebook.py
+++ Facebook.py
@@ -16,7 +16,7 @@ class Facebook_login(unittest.TestCase):
 		password = driver.find_element_by_xpath("//*[@id='pass']")
 		password.send_keys("9603982107")
 		Facebook_loginn_button = driver.find_element_by_xpath("//*[@id='u_0_m']")
-		login_button.click()
+		Facebook_loginn_button.click()
 	def tearDown(self):
 		self.driver.close()
 if __name__ == "__main__" :
